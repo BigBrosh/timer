ef839ef9f9303a62c04fb11e18c15413e46109ba
BigBrosh <30599468+BigBrosh@users.noreply.github.com>
2018-02-24 12:27:27 +0200

    timer modified (parametrs now adding through object)

--- project/timer.js
+++ project/timer.js
@@ -1,12 +1,17 @@
 class Timeleft {
-	constructor(target, parent, mainWrap) {
-		this.block = document.createElement('div');
-		this.block.className = parent;
+	constructor(obj) {
+		for (var key in obj) {
+			this[key + 'Name'] = obj[key];
+		}
 
-		this.mainWrap = document.querySelector(mainWrap) || document.getElementsByClassName('main')[0];
+		this.target = this.targetName;
+		this.block = document.createElement('div');
+		this.block.className = this.parentName || 'output';
+
+		this.mainWrap = document.querySelector(this.mainWrapName) || document.getElementsByClassName('main')[0];
 
 		this.mainWrap.appendChild(this.block);
-		this.runTimer(target, this.block);
+		this.runTimer(this.target, this.block);
 	}
 	
 	runTimer(target, parent) {
@@ -19,26 +24,36 @@ class Timeleft {
 			this.seconds = this.difference/1000;
 			this.minutes = this.seconds/60;
 			this.hours = this.minutes/60;
+			this.days = this.hours/24;
 
 			this.seconds = this.seconds%60;
 			this.minutes = this.minutes%60;
 			this.hours = this.hours%24;
+			this.days = this.days%365;
 
 			this.seconds = parseInt(this.seconds);
 			this.minutes = parseInt(this.minutes);
 			this.hours = parseInt(this.hours);
+			this.days = parseInt(this.days);
 
 			this.seconds = this.seconds > 9 ? this.seconds : '0' + this.seconds;
 			this.minutes = this.minutes > 9 ? this.minutes : '0' + this.minutes;
 			this.hours = this.hours > 9 ? this.hours : '0' + this.hours;
 
-			parent.innerHTML = `timeleft: ${this.hours} : ${this.minutes} : ${this.seconds}`;
+			parent.innerHTML = `timeleft: ${this.days} : ${this.hours} : ${this.minutes} : ${this.seconds}`;
 		}, 1000);
 	}
 }
 
-var target = new Date(2018, 1, 22, 22, 21, 60, 0);
-var timeleft = new Timeleft(target, 'output');
+var target = new Date(2018, 1, 25, 22, 21, 60, 0);
+var timeleft = new Timeleft({
+	target: target, 
+	parent: 'output'
+});
 
-var target2 = new Date(2018, 1, 23, 20, 32, 60, 0);
-var timeleft2 = new Timeleft(target2, 'second_output', '.timer_wrap');
+var target2 = new Date(2018, 1, 24, 20, 32, 60, 0);
+var timeleft2 = new Timeleft({
+	target: target2, 
+	parent: 'second_output', 
+	mainWrap: '.timer_wrap'
+});
